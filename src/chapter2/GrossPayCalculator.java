88e6421262de6b5759203a33241470f3860f6ff2
dj1ch <tg.1ch0712@gmail.com>
2024-06-11 18:45:17 -0700

    typo

--- src/chapter2/GrossPayCalculator.java
+++ src/chapter2/GrossPayCalculator.java
@@ -6,7 +6,7 @@ public class GrossPayCalculator {
 
     public static void main(String arg[]) {
         // 1. Get the number of hours worked
-        System.out.println("Enter the number of hours the employee worked."); // note: statement ends in semicolor
+        System.out.println("Enter the number of hours the employee worked."); // note: statement ends in semicolon
         Scanner scanner = new Scanner(System.in); // instance of scanner class
         int hours = scanner.nextInt(); // note: we must store data in a type
 
